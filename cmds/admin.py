1560c00bbfdf443c9d82c3f895d5dc931325e57e
twasawang <twasawang@mlytics.com>
2021-07-24 17:42:54 +0800

    [why]develop[what]avoid admin cmds exception

--- cmds/admin.py
+++ cmds/admin.py
@@ -1,6 +1,5 @@
 from discord.ext import commands
 from core.cog import CogExtension
-
 from tools.log import logger
 
 
@@ -8,21 +7,30 @@ class Admin(CogExtension):
     @commands.has_permissions(administrator=True)
     @commands.command()
     async def load(self, ctx, extension_name):
-        self.bot.load_extension(f'cmds.{extension_name}')
-        await ctx.send(f'loaded {extension_name} done.')
+        try:
+            self.bot.load_extension(f'cmds.{extension_name}')
+            await ctx.send(f'loaded {extension_name} done.')
+        except Exception as e:
+            logger.warning(str(e))
 
     @commands.has_permissions(administrator=True)
     @commands.command()
     async def unload(self, ctx, extension_name):
-        self.bot.unload_extension(f'cmds.{extension_name}')
-        await ctx.send(f'unloaded {extension_name} done.')
+        try:
+            self.bot.unload_extension(f'cmds.{extension_name}')
+            await ctx.send(f'unloaded {extension_name} done.')
+        except Exception as e:
+            logger.warning(str(e))
 
     @commands.has_permissions(administrator=True)
     @commands.command()
     async def reload(self, ctx, extension_name):
-        # if ctx.message.author.guild_permissions.administrator:
-        self.bot.reload_extension(f'cmds.{extension_name}')
-        await ctx.send(f'reload {extension_name} done.')
+        try:
+            # if ctx.message.author.guild_permissions.administrator:
+            self.bot.reload_extension(f'cmds.{extension_name}')
+            await ctx.send(f'reload {extension_name} done.')
+        except Exception as e:
+            logger.warning(str(e))
 
 
 def setup(bot):
--- cmds/langrisser.py
+++ cmds/langrisser.py
@@ -13,6 +13,7 @@ middle_name = 'langrisser'
 search_uri = f'{base_uri}/{middle_name}/index.php'
 cc = OpenCC('t2s')
 
+
 class Langrisser(CogExtension):
     @commands.command()
     async def q(self, ctx):
@@ -22,16 +23,17 @@ class Langrisser(CogExtension):
         try:
             response_obj = requests.get(search_uri, params={'search': cc.convert(key_word)})
         except Exception as e:
-            logger.info(str(e))
+            logger.warning(str(e))
+            return ctx.send('Langrisser wiki error')
         if response_obj.status_code != 200:
-            await ctx.send('langrisser wiki error')
+            await ctx.send('Langrisser wiki error')
         if response_obj.url.startswith(f'{base_uri}/{middle_name}'):
             await ctx.send(f'{urllib.parse.unquote(response_obj.url)}')
             return
         if result_uri := parse_langrisser_wiki_search_result(response_obj.text):
             await ctx.send(f'{base_uri}{urllib.parse.unquote(result_uri)}')
             return
-        await ctx.send('找不到')
+        await ctx.send('query without result')
 
 
 def setup(bot):
